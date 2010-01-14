6080a5d5684109fc52c3430ca3867ba92433217b
Ivan Nemytchenko <van.omsk@gmail.com>
2010-01-14 23:20:33 +0600

    <script> wrapper

--- lib/haml/filters/coffee.rb
+++ lib/haml/filters/coffee.rb
@@ -3,7 +3,23 @@ module Haml::Filters::Coffee
 
   lazy_require 'coffee-script'
 
-  def render(text)
-    CoffeeScript.compile(text)
+  def render_with_options(text, options)
+    <<END
+<script type=#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}>
+  //<![CDATA[
+    #{CoffeeScript.compile(text)}
+  //]]>
+</script>
+END
   end
+
+  #   def render(text)
+  #             <<END
+  # <script type=#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}>
+  #   //<![CDATA[
+  #     #{CoffeeScript.compile(text)}
+  #   //]]>
+  # </script>
+  #     END    
+  #   end
 end
